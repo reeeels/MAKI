7db0661f92cc88cf5d431963687eb614bfd4ae3e
Temiloluwa Ige <igetemiloluwa05@icloud.com>
2023-04-05 11:00:20 -0500

    feat: added results table

--- pages/table.tsx
+++ pages/table.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
+import styles from '@/styles/index.module.css';
 
-const Table = (data) => {
-  const tableHeaders = Object.keys(data);
+const Table = (props) => {
+    const tmp = props.data ? props.data : null;
+    const parsed = props.data ? [JSON.parse(props.data)] : [];
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          {tableHeaders.map(header => <th key={header}>{header}</th>)}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, index) => (
-          <tr key={index}>
-            {tableHeaders.map((header, index) => <td key={index}>{row[header]}</td>)}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+    return (
+        <div>
+            {tmp && parsed.map((index) => {
+                return (
+                    <>
+                        <div>
+                            <div className='d-flex flex-row'>
+                                <div className={styles.startbox}>
+                                    <h4 className={styles.label}>SYMPTOMS</h4>
+                                </div>
+
+                                <h4 className={styles.text}>{index.SYMPTOMS}</h4>
+                            </div>
+                            <div className='d-flex flex-row'>
+                                <div className={styles.box}>
+                                    <h4 className={styles.label}>MEDICAL HISTORY</h4>
+                                </div>
+                                <h4 className={styles.text}>{index.MEDICAL_HISTORY}</h4>
+                            </div>
+                            <div className='d-flex flex-row'>
+                                <div className={styles.box}>
+                                    <h4 className={styles.label}>PHYSICAL EXAM</h4>
+                                </div>
+                                <h4 className={styles.text}>{index.PHYSICAL_EXAM}</h4>
+                            </div>
+                            <div className='d-flex flex-row'>
+                                <div className={styles.box}>
+                                    <h4 className={styles.label}>MEDICATION</h4>
+                                </div>
+                                <h4 className={styles.text}>{index.MEDICATION !== '' ? index.MEDICATION : 'None'}</h4>
+                            </div>
+                            <div className='d-flex flex-row'>
+                                <div className={styles.endbox}>
+                                    <h4 className={styles.label}>TREATMENT</h4>
+                                </div>
+                                <h4 className={styles.text}>{index.TREATMENT}</h4>
+                            </div>
+                        </div>
+                    </>
+                )
+            })}
+
+        </div>
+    )
+}
 
 export default Table;
